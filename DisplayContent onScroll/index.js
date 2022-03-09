ee37e213cd9bb8eae5fe5e11a9554a098cd01f9b
awalaj <94739236+awalaj@users.noreply.github.com>
2022-03-09 10:13:24 +0700

    Update index.js

--- DisplayContent onScroll/index.js
+++ DisplayContent onScroll/index.js
@@ -1,16 +1,22 @@
 $(document).ready(function(){
-    window.onresize = function(){
-        console.log(window.outerWidth)
-    }
-    console.log($("img:nth-child(1)").attr("src"))
-    window.addEventListener("scroll", function(){
-        if(window.pageYOffset >= 265){
-            $("figure").animate({
-                "opacity" : "1",
-                "top" : "430px",
-                "left" : "200px",
-                "display" : "block"
-            }, 3000)
-        }
-    })
-})
+            window.addEventListener("scroll", function(){   
+                if(window.pageYOffset >= 265){
+                    $("figure").animate({
+                        "opacity" : "1",
+                        "top" : "430px",
+                        "left" : "200px",   
+                        "display" : "block"
+                    }, 1000, function(){
+                        if(window.pageYOffset >= 800){
+                            $("figure").animate({
+                                "opacity" : "0",
+                                "top" : "430px",
+                                "left" : "0px",   
+                                "display" : "none"
+                            }) 
+                            console.log("Halo")
+                        }
+                    })
+                }
+            })
+        })
